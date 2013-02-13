86b659a65f658f1560a5122336462232d7d477f0
Mark Grebe <mark.grebe@colorado.edu>
2013-02-12 21:06:41 -0600

    Initial cut at home page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+    subject { page }
+
+    describe "Home page" do
+        before { visit root_path }
+
+        it { should have_selector('h1',    text: 'Calendaring Application') }
+        it { should have_selector('title', text: 'CalendarApp') }
+    end
+end
