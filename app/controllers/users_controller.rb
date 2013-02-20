1f1599b66196c09fb4d185c103baed8f64ad913d
Mark Grebe <mark.grebe@colorado.edu>
2013-02-19 19:42:51 -0600

    Added password changing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,11 +27,23 @@ class UsersController < ApplicationController
     end
 
     def update
-        params[:user].each do |name, value|
-            @user.update_column(name, value)
+        # Settings Update (non-password)
+        if params[:user][:password] == nil
+            params[:user].each do |name, value|
+                @user.update_column(name, value)
+            end
+            flash[:success] = "Settings updated"
+            redirect_to @user
+        # Password Update
+        else
+            if @user.update_attributes(params[:user])
+                flash[:success] = "Settings updated"
+                sign_in @user
+                redirect_to @user
+            else
+                render 'edit'
+            end
         end
-        flash[:success] = "Settings updated"
-        redirect_to @user
     end
 
   private
