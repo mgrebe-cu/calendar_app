cb73c9bd781b56bc01c842170c940f744f874854
Mark Grebe <mark.grebe@colorado.edu>
2013-03-06 16:24:12 -0600

    Cleanup of javascript

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){ 
-    $('.day_cal_div').attr("scrollTop",200);
+    // Enable bootstrap popovers
     $('.event-popover').popover();
+    // Set the time fields to be time pickers
     $('#event_start_time').timepicker({minuteStep: 5, defaultTime: '10:00 AM'});
     $('#event_end_time').timepicker({minuteStep: 5, defaultTime: '11:00 AM'});
+    // Set the date fields to be date pickers
     $('.datepicker').datepicker({autoclose: true, todayBtn: true});
+    // Set end time to an hour after start time when exiting start time
     $('#event_start_time').change(function() {
         var hoursToAdd = 1;
         var oldTime = Date.parse('Jan 1, 2009 ' + $('#event_start_time').val());
@@ -35,6 +38,7 @@ $(document).ready(function(){
     $('#event_start_date').change(function() {
         $('#event_end_date').val($('#event_start_date').val());
     });
+    // En/Disable start/end time based on state of All Day checkbox
     $('#event_all_day').click(function() {
         if (!this.checked) {
             $('#event_start_time').attr('disabled',false);
@@ -47,7 +51,11 @@ $(document).ready(function(){
         }
     });
     $('.day_div').scrollTop(340);
-    $("#event-form").validate({
+    // Client side validation for event form
+    var eventValidator = $("#event-form").validate({
+        errorContainer: "#event_errorbox",
+        errorLabelContainer: "#event_errorbox ul",
+        wrapper: "li",
         rules: {
            "event[title]": {
                 required:true
@@ -57,8 +65,27 @@ $(document).ready(function(){
             },
            "event[end_date]": {
                 required:true
+            }},
+        messages: {
+           "event[title]": "Title is required",
+           "event[start_date]": "Start Date is required",
+           "event[end_date]": "End Date is required"
             }
-    }
+    });
+    // Clearing the event form for new event
+    $("#new_event").click(function() {
+        $('#event_title').val("");
+        $('#event_location').val("");
+        $('#event_start_date').val("");
+        $('#event_end_date').val("");
+        $('#event_all_day').attr('checked', false);
+        $('#event_start_time').attr('enabled',true);
+        $('#event_end_time').attr('enabled',true);
+        //$('#event_start_time').val("");
+        //$('#event_end_time').val("");
+        $('#message_area').val("");
+        eventValidator.resetForm();
+        eventValidator.submit = {};
     });
 
 }) ;
