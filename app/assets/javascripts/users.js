f9eb30366be66fc642b0278d31349246017744f4
Mark Grebe <mark.grebe@colorado.edu>
2013-03-07 14:43:32 -0600

    Add new user validator

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -49,4 +49,55 @@ $(document).ready(function(){
            }
        }
     });
+
+    $.validator.addMethod("validPassword", function(value) {
+        var passRe = /^.*(?=.*\d)(?=.*[a-zA-Z]).*$/;
+        return passRe.test(value);
+        }, $.validator.format("Password must contain a letter and a number")); 
+
+    var passwordValidator = $("#user-form").validate({
+        errorContainer: "#user_errorbox",
+        errorLabelContainer: "#user_errorbox ul",
+        wrapper: "li",
+        rules: {
+           "user[full_name]": {
+                required:true,
+                maxlength: 50
+            },
+           "user[username]": {
+                required:true,
+                maxlength: 20
+            },
+           "user[password]": {
+                required:true,
+                minlength: 6,
+                maxlength: 20,
+                validPassword: true
+            },
+           "user[password_confirmation]": {
+                required:true,
+                equalTo: $("#user_password")
+            }
+         },
+         messages: {
+           "user[full_name]": {
+                required:"Full Name is required",
+                maxlength:"Full Name must be less than 50 characters"
+           },
+           "user[username]": {
+                required:"Username is required",
+                maxlength:"Username must be less than 20 characters"
+           },
+           "user[password]": {
+                required:"Password is required",
+                minlength:"Password must be greater than 6 characters",
+                maxlength:"Password must be less than 20 characters",
+                validPassword:"Password must contain a letter and a number"
+           },
+           "user[password_confirmation]": {
+                required:"Password Confirmation is required",
+                equalTo:"Password and Password Confirmation must match"
+           }
+       }
+    });
 }) ;
