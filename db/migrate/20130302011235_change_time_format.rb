54d6fcf00a8eeaecc7b32f0750b6da87fd8f9438
Mark Grebe <mark.grebe@colorado.edu>
2013-03-09 20:06:49 -0600

    Fix migrations...again

--- db/migrate/20130302011235_change_time_format.rb
+++ db/migrate/20130302011235_change_time_format.rb
@@ -1,9 +1,9 @@
 class ChangeTimeFormat < ActiveRecord::Migration
   def change
-    remove_column :events, :start, :datetime
-    remove_column :events, :end, :datetime
-    add_column :events, :start
-    add_column :events, :end
+    remove_column :events, :start
+    remove_column :events, :end
+    add_column :events, :start, :datetime
+    add_column :events, :end, :datetime
     remove_column :events, :date
   end
 end
