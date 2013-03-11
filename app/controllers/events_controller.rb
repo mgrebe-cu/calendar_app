0087e05c081cdb3e6471b355d57ad90280a6bddc
Mark Grebe <mark.grebe@colorado.edu>
2013-03-10 21:33:38 -0500

    Fix Daylight Savings time

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,12 @@ class EventsController < ApplicationController
     before_filter :correct_user,   only: :destroy
 
     def parse_params
+        # Find the offset so we store times in UTC
         offset = Time.zone.now.time_zone.utc_offset
+        # Make sure and check if our timezone currently has daylight savings time
+        if Time.zone.now.dst?
+            offset = offset + 60*60
+        end
         begin
             if @event.all_day
                 start_t = params[:event][:start_date]
