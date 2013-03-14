bab423728cbadfabfa6d9ff9002dd9f0e626db0a
Mark Grebe <mark.grebe@colorado.edu>
2013-03-13 21:49:51 -0500

    Add colors to month events

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -1,2 +1,38 @@
+CALF_CLASS = Hash.new("calf_blue")
+CALF_CLASS[0] = "calf_blue"
+CALF_CLASS[1] = "calf_red"
+CALF_CLASS[2] = "calf_orange"
+CALF_CLASS[3] = "calf_yellow"
+CALF_CLASS[4] = "calf_green"
+CALF_CLASS[5] = "calf_purple"
+CALF_CLASS[6] = "calf_brown"
+
+CALB_CLASS = Hash.new("calb_blue")
+CALB_CLASS[0] = "calb_blue"
+CALB_CLASS[1] = "calb_red"
+CALB_CLASS[2] = "calb_orange"
+CALB_CLASS[3] = "calb_yellow"
+CALB_CLASS[4] = "calb_green"
+CALB_CLASS[5] = "calb_purple"
+CALB_CLASS[6] = "calb_brown"
+
 module CalendarsHelper
+    def calf_class(color)
+        if color.nil?
+            return CALF_CLASS[0]
+        else
+            return CALF_CLASS[color.value]
+        end
+    end
+
+    def calb_class(color)
+        if color.nil?
+            return CALB_CLASS[0]
+        else
+            return CALB_CLASS[color.value]
+        end
+    end
+
 end
+
+
