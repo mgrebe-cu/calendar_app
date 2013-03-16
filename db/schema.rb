fbcd9c413011cbac86caeaac2bbdabb540f8a370
Mark Grebe <mark.grebe@colorado.edu>
2013-03-16 09:46:01 -0500

    Added row to subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316002447) do
+ActiveRecord::Schema.define(:version => 20130316144101) do
 
   create_table "calendars", :force => true do |t|
     t.boolean  "default"
