8c386f604f0b334e0002a070223da4c86b534cab
Mark Grebe <mark.grebe@colorado.edu>
2013-03-21 22:08:39 -0500

    Fixed description

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -3,7 +3,7 @@ class Calendar < ActiveRecord::Base
     enumerize :color, in: {blue: 0, red: 1, orange: 2, yellow: 3,
                            green: 4, purple: 5, brown: 6}, default: :blue
 
-    attr_accessible :default, :title, :description, :color, :displayed, :description, :public
+    attr_accessible :default, :title, :description, :color, :displayed, :public
 
     belongs_to :user
     has_many :events, dependent: :destroy
